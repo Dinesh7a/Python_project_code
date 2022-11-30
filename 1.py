4f1aafd91bf4c5b8eb395e9e4e48f6144bbf74a9
Dinesh7a <119056413+Dinesh7a@users.noreply.github.com>
2022-11-30 21:36:52 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+#sum of numbers from 1 to 25, 50 to 75, 90 to 100
+
+def sum(x,y):
+    s=0;
+    for i in range(x,y+1):
+        s=s+i;
+    print("sum of integers",s)
+max(1,25)
+max(50,75)
+max(90,100)
